7eb1defa272d459eae178c1935261267d5eefdc9
csucoderlee <csucoderlee@foxmail.com>
2021-07-10 22:42:12 +0800

    first commit

---
+++ src/ch2/constant_test/constant_try_test.go
@@ -0,0 +1,20 @@
+package constant_test
+
+import "testing"
+
+const (
+	Monday = 1 + iota
+	Tuesday
+	Wednesday
+)
+
+const (
+	Readable = 1 << iota
+	Writable
+	Executable
+)
+
+func TestConstant(t *testing.T) {
+	t.Log(Monday, Tuesday, Wednesday)
+	t.Log(Readable, Writable, Executable)
+}
---
+++ src/ch2/fib/fib_test.go
@@ -0,0 +1,25 @@
+package fib
+
+import (
+	"testing"
+)
+
+func TestFib(t *testing.T) {
+
+	a := 1
+	b := 1
+	t.Log(a)
+	for i := 0; i < 6; i++ {
+		t.Log(b)
+		temp := a
+		a = b
+		b = temp + a
+	}
+}
+
+func TestExchange(t *testing.T) {
+	a := 1
+	b := 2
+	a,b = b, a
+	t.Log(a, b)
+}
