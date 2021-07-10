4545a013d69765b7e60c2004b454dd0d89db2310
csucoderlee <csucoderlee@foxmail.com>
2021-07-10 22:50:59 +0800

    测试位运算

--- src/ch2/constant_test/constant_try_test.go
+++ src/ch2/constant_test/constant_try_test.go
@@ -18,3 +18,8 @@ func TestConstant(t *testing.T) {
 	t.Log(Monday, Tuesday, Wednesday)
 	t.Log(Readable, Writable, Executable)
 }
+
+func TestConstant1(t *testing.T) {
+	a := 7 //0111
+	t.Log(a&Readable == Readable, a&Writable == Writable, a&Executable == Executable)
+}
