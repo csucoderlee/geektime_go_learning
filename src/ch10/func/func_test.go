454b46fa59199bab9bedaef0be4d5528877441f4
csucoderlee <csucoderlee@foxmail.com>
2021-07-20 12:56:07 +0800

    函数

---
+++ src/ch10/func/func_test.go
@@ -0,0 +1,52 @@
+package func_test
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestFunc(t *testing.T) {
+
+	result1, result2 := multiResult()
+	t.Log(result1, result2)
+
+	result3 := paramFuc(func(op int) int {
+		return op * 3
+	})
+	t.Log(result3)
+
+	result4 := paramFuc(func(op int) int {
+		return op * 4
+	})
+	t.Log(result4)
+
+	result5 := paramFuc(funcAsParam)
+	t.Log(result5)
+}
+
+//函数可以有多个返回值
+func multiResult() (int, int) {
+	return rand.Int(), rand.Int()
+}
+
+func paramFuc(inner func(op int) int) int {
+	return inner(1)
+}
+
+// funcAsParam函数可以作为 paramFuc的参数， 见上方result5
+func funcAsParam(op int) int {
+	return op * 5
+}
+
+func TestSomeParam(t *testing.T) {
+	t.Log(someParam(1, 2, 3, 4))
+	t.Log(someParam(1, 2, 3))
+
+}
+func someParam(op ...int) int {
+	result := 0
+	for _, value := range op {
+		result += value
+	}
+	return result
+}
